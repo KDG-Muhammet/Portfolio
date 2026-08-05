8a3814d30b71af931a389a19283ae7f091cbf5c6
muhammet murat <muhammet.murat@student.kdg.be>
2026-08-05 22:03:29 +0200

    adding translation to components

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal, inject } from '@angular/core';
+import {TranslatePipe, TranslateDirective, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  private translate = inject(TranslateService);
+  currentLanguage = 'en';
 
-}
+  useLanguage(language: string): void {
+    
+      this.currentLanguage = language;
+      this.translate.use(language);
+  }
+} 
